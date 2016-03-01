8de9d94be2f0a77b42f694398d68a9fe5e66f0f9
francoisdefever <fdefever.work@gmail.com>
2016-03-01 10:16:38 +0100

    add Keypress event | Messages definition : value, timestamp, user(bugged)

--- live-chat.js
+++ live-chat.js
@@ -3,9 +3,24 @@ Messages = new Mongo.Collection('messages');
 if (Meteor.isClient) {
   Template.messages.helpers({
     messages: function () {
-      return Messages.find();
+      return Messages.find({});
     }
   });
+
+  Template.messages.events({
+    'keypress textarea': function(e, instance) {
+      if (e.keyCode == 13) { //enter key pressed
+        var value = instance.find('textarea').value;
+        instance.find('textarea').value = '';
+
+        Messages.insert({
+          message: value,
+          timestamp: new Date (),
+          // user: Meteor.userId()
+        })
+      }
+    }
+  })
 }
 
 if (Meteor.isServer) {
